239c2ed3921f48740c08454f6b1b6a097559e100
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-07-26 14:44:12 +1000

    migrate test.js and faunadb.js to src/functions

---
+++ src/functions/faunadb.js
@@ -0,0 +1,36 @@
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const FAUNA_ACCESS_TOKEN = process.env.FAUNA_ACCESS_TOKEN;
+
+const query = `
+query {
+  allProducts {
+    data {
+      name
+    }
+  }
+}`;
+
+const fauna = () =>
+  new Promise((resolve, reject) => {
+    fetch("https://graphql.fauna.com/graphql", {
+      method: "POST",
+      body: JSON.stringify({ query }),
+      headers: {
+        Authorization: `Bearer ${FAUNA_ACCESS_TOKEN}`,
+      },
+    })
+      .then((res) => res.text())
+      .then((body) => body)
+      .catch(() => reject("Failed to fetch query results"));
+  });
+
+export async function handler(event, context) {
+  const body = await fauna();
+
+  return {
+    statusCode: 200,
+    body,
+  };
+}
